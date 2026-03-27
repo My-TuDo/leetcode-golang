5f97ce99b38fe4a7770c27e1f8d1bf05d7e1b98c
My-TuDo <2647601953@qq.com>
2026-03-27 21:13:46 +0800

    alg: Add maxProfit II greedy solution(LeetCode 122)

---
+++ 122.买卖股票的最佳时机-ii.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=122 lang=golang
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	// 贪心算法：只要后一天的价格比前一天高，就进行买入和卖出操作
+	max := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			max += prices[i] - prices[i-1]
+		}
+	}
+	return max
+
+}
+
+// @lc code=end
+
