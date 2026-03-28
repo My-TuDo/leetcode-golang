0739c30ca096cb3367b97f220e9e86b4924e37e1
My-TuDo <2647601953@qq.com>
2026-03-28 16:13:44 +0800

    alg: Add majorityElement solution for LeetCode 169(Boyer-Moore)

---
+++ 169.多数元素.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=golang
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+func majorityElement(nums []int) int {
+	// 摩尔投票法
+	candidate := 0
+	count := 0
+
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+		if num == candidate {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return candidate
+}
+
+// @lc code=end
+
