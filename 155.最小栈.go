4f18834c589ce7f6ea4c9c73582464297b5caa71
My-TuDo <2647601953@qq.com>
2026-04-06 20:03:24 +0800

    feat: implement MinStack with O(1) getMin using auxiliary stack

---
+++ 155.最小栈.go
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=golang
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+// minStack用于记录最小值。
+func Constructor() MinStack {
+	return MinStack{
+		stack:    []int{},
+		minStack: []int{},
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+
+	// 如果minStack栈为空，则入栈，否则对已有的数进行比较。
+	if len(this.minStack) == 0 || val <= this.minStack[len(this.minStack)-1] {
+		this.minStack = append(this.minStack, val)
+	} else {
+		this.minStack = append(this.minStack, this.minStack[len(this.minStack)-1])
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
+// @lc code=end
+
