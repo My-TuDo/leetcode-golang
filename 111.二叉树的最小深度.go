9cee7092942b423b589aef60c43a331ec4ed422c
My-TuDo <2647601953@qq.com>
2026-04-07 14:42:40 +0800

    feat: solve LC111 Minimum Depth of Binary Tree using BFS (n)$

---
+++ 111.二叉树的最小深度.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=golang
+ *
+ * [111] 二叉树的最小深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+	// BFS
+	if root == nil {
+		return 0
+	}
+	var queue []*TreeNode
+	queue = append(queue, root)
+	depth := 1
+
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			if node.Left == nil && node.Right == nil {
+				return depth
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		depth++
+	}
+	return depth
+}
+
+// @lc code=end
+
