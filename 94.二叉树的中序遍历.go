4dd9c63244759facfa367550be12d4a39e2933c6
My-TuDo <2647601953@qq.com>
2026-04-07 14:30:59 +0800

    feat: solve LC94 Binary Tree Inorder Traversal using iterative stack (n)$

---
+++ 94.二叉树的中序遍历.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	// 迭代法
+	var res []int
+	var stack []*TreeNode
+	curr := root
+
+	for curr != nil || len(stack) > 0 {
+		for curr != nil {
+			stack = append(stack, curr)
+			curr = curr.Left
+		}
+		curr = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, curr.Val)
+		curr = curr.Right
+	}
+	return res
+}
+
+// @lc code=end
+
