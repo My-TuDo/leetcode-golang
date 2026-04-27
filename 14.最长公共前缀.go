cd134b840960d0a5865e203b921c27cb88c2869c
My-TuDo <2647601953@qq.com>
2026-04-27 21:30:07 +0800

    chore: review leetcode No.14 (Longest Common Prefix)

--- 14.最长公共前缀.go
+++ 14.最长公共前缀.go
@@ -6,14 +6,16 @@
 
 // @lc code=start
 func longestCommonPrefix(strs []string) string {
+	// 复习：最长公共前缀
 	if len(strs) == 0 {
 		return ""
 	}
+	// 纵向查找：逐字符对比
 	first := strs[0]
 	for i := 0; i < len(first); i++ {
 		char := first[i]
 		for j := 1; j < len(strs); j++ {
-			if i == len(strs[j]) || strs[j][i] != char {
+			if i >= len(strs[j]) || strs[j][i] != char {
 				return first[:i]
 			}
 		}
